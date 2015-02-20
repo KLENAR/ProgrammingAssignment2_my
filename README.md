21f926a1caaa2cb40a4353a6a543d1853bc5f1a2
KLENAR <klenar2001@mail.ru>
2015-02-20 23:22:34 +0300

    Final variant
    Final variant

--- README.md
+++ README.md
@@ -3,42 +3,5 @@
 ## 2. get the value of the matrix
 ## 3. set the value of inverse of the matrix 4. get the value of inverse of the matrix
 ## Second function returns the inverse of the matrix. 
-##  It first checks if the matrix has  been changed. 
-## If so, it stops, gets the message and skips the computation.
-## If not,  it checks if the inverse has already been computed. 
-##  If so, it gets the result and skips the  computation.
-## If not, it computes the inverse, sets the value in the cache.
-## This function assumes that the matrix is always invertible.
-## Example
-## x<-matrix(c(1,2,3,4),2,3)
-## > x
-##     [,1] [,2]
-## [1,]    1    3
-## [2,]    2    4
-## > mmx<-makeCacheMatrix(x)
-## mmx$get()
-##     [,1] [,2]
-## [1,]    1    3
-## [2,]    2    4
-## > mmx$getinverse()
-## NULL
-## > ccacheSolve(mmx,x)
-##      [,1] [,2]
-##  [1,]   -2  1.5
-## [2,]    1 -0.5
-## > x<-matrix(c(10,2,3,4),2,2)
-## > x
-##      [,1] [,2]
-##  [1,]   10    3
-##  [2,]    2    4
-## > ccacheSolve(mmx,x)
-## Error in ccacheSolve(mmx, x) : please reset matrix
-## > mmx$set(x)
-## > mmx$get()
-##      [,1] [,2]
-##  [1,]   10    3
-##  [2,]    2    4
-## > ccacheSolve(mmx,x)
-##             [,1]        [,2]
-##  [1,]  0.11764706 -0.08823529
-## v[2,] -0.05882353  0.29411765
+## This function assumes that the matrix is always invertible 
+## If matrix changes it needs e.g. recache it   before call this function
--- cachematrix.R
+++ cachematrix.R
@@ -20,58 +20,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 # The following function returns the inverse of the matrix. 
-# It first checks if the matrix has  been changed (dimentions or elements). 
-# If so, it stops, gets the message and skips the computation.
-# If not,  it checks if the inverse has already been computed. 
-# If so, it gets the result and skips the  computation.
-# If not, it computes the inverse, sets the value in the cache via
-# setinverse function.
-# This function assumes that the matrix is always invertible.
+# This function assumes that the matrix is always invertible 
+# If matrix changes it needs e.g. recache it   before call this function
 
-ccacheSolve <- function(x, amx=matrix(), ...) {
-  data <- x$get()
-  if(!isTRUE(all.equal(dim(amx),dim(data)))) {stop("please reset matrix")}
-  if(!isTRUE(all.equal(amx,data)))  {stop("please reset matrix")}
-     inv <- x$getinverse()
-     if(!is.null(inv)) {
-       message("getting cached data.")
+ccacheSolve <- function(x,...) {
+    inv<-x$getinverse()
+    if(!is.null(inv)) {
+       # getting cached data
        return(inv)
      }
+    # solving inverse
+     data <- x$get()
      inv <- solve(data)
      x$setinverse(inv)
      inv
 }
-## Example
-## x<-matrix(c(1,2,3,4),2,3)
-## > x
-##     [,1] [,2]
-## [1,]    1    3
-## [2,]    2    4
-## > mmx<-makeCacheMatrix(x)
-## mmx$get()
-##     [,1] [,2]
-## [1,]    1    3
-## [2,]    2    4
-## > mmx$getinverse()
-## NULL
-## > ccacheSolve(mmx,x)
-##      [,1] [,2]
-##  [1,]   -2  1.5
-## [2,]    1 -0.5
-## > x<-matrix(c(10,2,3,4),2,2)
-## > x
-##      [,1] [,2]
-##  [1,]   10    3
-##  [2,]    2    4
-## > ccacheSolve(mmx,x)
-## Error in ccacheSolve(mmx, x) : please reset matrix
-## > mmx$set(x)
-## > mmx$get()
-##      [,1] [,2]
-##  [1,]   10    3
-##  [2,]    2    4
-## > ccacheSolve(mmx,x)
-##             [,1]        [,2]
-##  [1,]  0.11764706 -0.08823529
-## v[2,] -0.05882353  0.29411765
+
 
